dff6a7fc9291b96f3a0541da0791aad10c78f727
unknown <leonardo_wolff@outlook.com>
2024-07-25 22:28:06 -0300

    Adicionando funcionalidades ao UsuarioDao

--- src/main/java/br/com/primeiroProjeto/modelo/dao/UsuarioDao.java
+++ src/main/java/br/com/primeiroProjeto/modelo/dao/UsuarioDao.java
@@ -1,9 +1,13 @@
 
 package br.com.primeiroProjeto.modelo.dao;
 import br.com.primeiroProjeto.modelo.conexao.ConexaoMySql;
+import br.com.primeiroProjeto.modelo.dominio.Perfil;
 import br.com.primeiroProjeto.modelo.dominio.Usuario;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 
 public class UsuarioDao {
@@ -18,6 +22,7 @@ public class UsuarioDao {
 
     private String adcionar(Usuario user) {
         String sql = "INSERT INTO usuario(nome, usuario, senha, perfil) VALUES(?,?,?,?)";
+
         try {
             PreparedStatement preparedStatement = conn.obterConexao().prepareStatement(sql);
             preenchendoValores(preparedStatement, user);
@@ -51,11 +56,60 @@ public class UsuarioDao {
         }
     }
     
-    public List<user> buscarTodosUsuarios(){
+    public List<Usuario> buscarTodosUsuarios(){
         String sql = "SELECT * FROM usuario";
+        List<Usuario> users = new ArrayList<>();
         try {
-            
-        } catch (Exception e) {
+            ResultSet result = conn.obterConexao().prepareStatement(sql).executeQuery();
+            while(result.next()){
+                users.add(getUsuario(result));
+            }
+        } catch (SQLException e) {
+            System.out.println(String.format("Error: ", e.getMessage()));
         }
+        return users;
+    }
+
+    private Usuario getUsuario(ResultSet result) throws SQLException{
+        Usuario usuario = new Usuario();
+
+        usuario.setID(result.getInt("id"));
+        usuario.setNome(result.getString("nome"));
+        usuario.setUsuário(result.getString("usuario"));
+        usuario.setSenha(result.getString("senha"));
+        usuario.setPerfil(result.getObject("perfil", Perfil.class));
+        usuario.setAtivo(result.getBoolean("ativo"));
+        usuario.setDataHoraCriacao(result.getObject("data_hora_criacao", LocalDateTime.class));
+        usuario.setUltimoLogin(result.getObject("ultimo_login", LocalDateTime.class));
+
+        return usuario;
+    }
+
+    public Usuario buscarUsuarioId(int ID){
+        String sql = "SELECT * FROM usuario where ID = " + ID;
+        List<Usuario> users = new ArrayList<>();
+        try {
+            ResultSet result = conn.obterConexao().prepareStatement(sql).executeQuery();
+            if(result.next()){
+                return  getUsuario(result);
+            }
+        } catch (SQLException e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+        return null;
+    }
+
+    public Usuario buscarUsuarioUsuario(String usuario){
+        String sql = "SELECT * FROM usuario where ID = " + usuario;
+        List<Usuario> users = new ArrayList<>();
+        try {
+            ResultSet result = conn.obterConexao().prepareStatement(sql).executeQuery();
+            if(result.next()){
+                return  getUsuario(result);
+            }
+        } catch (SQLException e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+        return null;
     }
 }
